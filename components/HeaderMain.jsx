79b5719a3afd3bca70a5fd5e5a599509f0dc8e5f
carlos-augustow <carlos_augusto_wallauer@outlook.com>
2022-11-18 08:44:55 -0300

    Update HeaderMain.jsx

--- components/HeaderMain.jsx
+++ components/HeaderMain.jsx
@@ -5,8 +5,8 @@ export default function HeaderMain() {
       return (
             <header className={styles.header}>
                   <div className={styles.imgs}>
-                        <Image src="/../public/imgs/logoNovo.png" alt="logo" layout="fill" />
-                        <Image src="/../public/imgs/cinedev_escrito_branco.png" layout="fill" alt="img" />
+                        <Image src="/imgs/logoNovo.png" alt="logo" layout="fill" />
+                        <Image src="/imgs/cinedev_escrito_branco.png" layout="fill" alt="img" />
                   </div>
                   <div className={styles.btnSair}>
                         <span>
