205a30ad8833ea202d14898e31c45b9c066e543a
carlos-augustow <85113496+carlos-augustow@users.noreply.github.com>
2022-11-18 00:35:21 -0300

    fan

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
                         <h2>Dispositivos compativeis:</h2>
                         <Image
                               className={styles.img_dispositivos}
-                              src="/../public/imgs/dispositivos.png"
+                              src="/https://camo.githubusercontent.com/add2c9721e333f0043ac938f3dadbc26a282776e01b95b308fcaba5afaf74ae3/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67"
                               // width={480}
                               // height={115}
                               layout="fill"
