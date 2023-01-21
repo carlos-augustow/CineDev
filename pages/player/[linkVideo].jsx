baca3b6904d57f48dbcbb89679739e1b51431071
carlos-augustow <85113496+carlos-augustow@users.noreply.github.com>
2023-01-21 00:14:00 -0300

    Update [linkVideo].jsx

--- pages/player/[linkVideo].jsx
+++ pages/player/[linkVideo].jsx
@@ -6,9 +6,11 @@ export default function VideoPlayer(props) {
   const router = useRouter();
   const link = router.query.linkVideo;
   //   console.log("link_router: ", link);
-
+  // pegar o width e height da tela
+  const { innerWidth: width, innerHeight: height } = window;
   return (
-    <video src={link}></video>
+    <video src={link} width={width} height={height} controls />
+
     // <div className={styles.main}>
     //       <SublimeVideo autoPlay loop src={link} />
 
