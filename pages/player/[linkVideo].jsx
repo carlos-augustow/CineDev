9b02009a6880b1cb44c02bd134fdae05fd2f5ca3
carlos-augustow <85113496+carlos-augustow@users.noreply.github.com>
2023-01-20 22:57:27 -0300

    Atualização pagina de video

--- pages/player/[linkVideo].jsx
+++ pages/player/[linkVideo].jsx
@@ -3,14 +3,15 @@ import styles from "../../styles/Player.module.scss";
 // import ReactPlayer from "react-player";
 import SublimeVideo from "react-sublime-video";
 export default function VideoPlayer(props) {
-      const router = useRouter();
-      const link = router.query.linkVideo;
-      //   console.log("link_router: ", link);
+  const router = useRouter();
+  const link = router.query.linkVideo;
+  //   console.log("link_router: ", link);
 
-      return (
-            <div className={styles.main}>
-                  <SublimeVideo autoPlay loop src={link} />
+  return (
+    <video src={link}></video>
+    // <div className={styles.main}>
+    //       <SublimeVideo autoPlay loop src={link} />
 
-            </div>
-      )
+    // </div>
+  );
 }
